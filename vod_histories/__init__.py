c4ce9234157a3d18f4bfdc277c7e36aa5ab62e9e
Khoa Pham <khoapd7@fpt.com.vn>
2018-07-16 15:45:11 +0700

    fix error MAX_VOD_HISTORY_PER_USER when add new data

--- vod_histories/__init__.py
+++ vod_histories/__init__.py
@@ -4,5 +4,7 @@ class Config:
 
     VOD_POST_TEMP = {'user_id': str(""), 'object_id': str(""), "episode_num": int(0), "elapsed_time": int(0)}
 
+    # Each user has max 100 vod
+    MAX_VOD_HISTORY_PER_USER = 100
     # 30 days
     VOD_HISTORY_TIMEOUT = 2592000
